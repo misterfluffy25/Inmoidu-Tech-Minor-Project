2480f23dd915d31f03e2b5fe1474567cbef28263
Kunwar Yashaswee Chhaunker <75092847+misterfluffy25@users.noreply.github.com>
2021-04-08 10:22:32 +0530

    Add files via upload

---
+++ immovidu.js
@@ -0,0 +1,13 @@
+function myLoading(){
+	alert("Page is ready to use!")
+}
+function myFunction() {
+  var x, text;
+  x = document.getElementById("numb").value;
+  if (isNaN(x) || x < 1 || x > 10) {
+    text = "Input not valid";
+  } else {
+    text = "Input OK";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
